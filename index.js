216a7c0a7139ad97fc13156e9c269208257f2b22
JakubKub13 <jakubkubala3@gmail.com>
2022-12-28 18:32:36 +0100

    Finishing simple API

---
+++ index.js
@@ -0,0 +1,45 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const configuration = new Configuration({
+    //organization: "org-NDtag0ExsqoEV8dgbuI1tVLH",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+//const response = await openai.listEngines();
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+const port = 3080;
+
+app.post('/', async (req, res) => {
+    const { message, currentModel } = req.body;
+    console.log(message, "message")
+    console.log(currentModel, "currentModel")
+    const response = await openai.createCompletion({
+        model: `${currentModel}`,
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
+    });
+    res.json({
+        message: response.data.choices[0].text
+    })
+})
+
+app.get('/models', async (req, res) => {
+   const response = await openai.listEngines();
+   res.json({
+        models: response.data.data
+   })
+    
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
